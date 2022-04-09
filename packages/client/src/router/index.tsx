016a97fd8d4d67e9632dbca17591d94265aa2e1e
k10wl <y.kunytskyy@gmail.com>
2022-04-09 16:11:24 +0300

    import improvement

--- packages/client/src/router/index.tsx
+++ packages/client/src/router/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 
-import Homepage from "src/pages/Homepage";
+import { Homepage, Catalog } from "src/pages";
 
 const Router: React.FC = () => (
   <Routes>
     <Route path="/" element={<Homepage />} />
+    <Route path="/catalog" element={<Catalog />} />
   </Routes>
 );
 
