7b1802a100562be12852fe3e99dafad93e1d2614
k10wl <y.kunytskyy@gmail.com>
2022-04-09 16:11:24 +0300

    enhanced focus-visible

--- packages/client/src/stylesheets/FocusVisible.ts
+++ packages/client/src/stylesheets/FocusVisible.ts
@@ -2,21 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 import { accentColor4 } from "./Colors";
 
-const FocusVisible = createGlobalStyle`
-  @keyframes pulse-animation {
-    0% {
-      transform: scale(70%);
-    }
-    100% {
-      transform: scale(80%);
-    }
-  }
-
-  *:focus-visible, * > *:focus-visible {
-    position: relative;
-    outline: none;
-    border: none;
-    &::after {
+const focusBase = `
       position: absolute;
       top: 0;
       left: 0;
@@ -26,9 +12,40 @@ const FocusVisible = createGlobalStyle`
       height: 100%;
       background-color: ${accentColor4};
       mix-blend-mode: lighten;
-      opacity: 50%;
+      opacity: 33%;
       border-radius: 50%;
-      animation: pulse-animation 2.5s infinite alternate;
+`;
+
+const FocusVisible = createGlobalStyle`
+  @keyframes before-pulse-animation {
+    0% {
+      transform: scale(70%);
+    }
+    100% {
+      transform: scale(80%);
+    }
+  }
+
+  @keyframes after-pulse-animation {
+    0% {
+      transform: scale(50%);
+    }
+    100% {
+      transform: scale(70%);
+    }
+  }
+
+  *:focus-visible, * > *:focus-visible {
+    position: relative;
+    outline: none;
+    border: none;
+    &::after {
+      ${focusBase};
+      animation: before-pulse-animation 2.5s infinite alternate;
+    }
+    &::before {
+      ${focusBase};
+      animation: after-pulse-animation 2.5s infinite alternate;
     }
   }
 `;
